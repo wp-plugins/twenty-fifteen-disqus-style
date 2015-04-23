907660699cef05ea6024fc25f3d4a48199bd8f61
abrudtkuhl <abrudtkuhl@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-23 02:37:20 +0000

    initial import from github
    git-svn-id: https://plugins.svn.wordpress.org/twenty-fifteen-disqus-style/trunk@1142794 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ twentyfifteen-disqus.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Plugin Name: Twenty Fifteen Disqus Comments Style
+ * Plugin URI: https://github.com/abrudtkuhl/twentyfifteen-disqus
+ * Description: Makes Disqus look nice in the Twenty Fifteen WordPress Theme
+ * Version: 0.1
+ * Author: Andy Brudtkuhl
+ * Author URI: http://youmetandy.com
+ */
+
+if( ! function_exists( 'twentyfifteen_disqus' ) ) {
+
+  function twentyfifteen_disqus() {
+    wp_enqueue_style( 'twentyfifteen-disqus', plugins_url( 'gravity-forms-bootstrap.css' ), array(), '1.0', 'all' );
+  }
+
+  add_action( 'wp_enqueue_scripts', 'twentyfifteen_disqus' );
+}
